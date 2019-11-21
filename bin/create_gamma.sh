666119c4f99f135fed0fbd28b73b09bcd52f1654
Adolfo Neto <adolfo.usp@gmail.com>
2019-11-21 14:41:57 -0300

    Add files via upload

---
+++ bin/create_gamma.sh
@@ -0,0 +1,5 @@
+./gamma -from 20 -to 600 -step 20
+./gamma -from 20 -to 600 -step 20 -l 10
+./gamma -from 20 -to 600 -step 20 -n
+./gamma -from 20 -to 600 -step 20 -n -l 10
+
---
+++ bin/create_h.sh
@@ -0,0 +1,2 @@
+./h -from 1 -to 9
+./h -from 1 -to 9 -n
